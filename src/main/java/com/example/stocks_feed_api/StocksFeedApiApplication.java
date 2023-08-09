7ddf00a52c133b0cf4ce2f5659be04257d5b3195
Wismut <codename__47@ukr.net>
2023-08-09 23:08:30 +0300

    init repo

---
+++ src/main/java/com/example/stocks_feed_api/StocksFeedApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.stocks_feed_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StocksFeedApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StocksFeedApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/stocks_feed_api/StocksFeedApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stocks_feed_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StocksFeedApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
